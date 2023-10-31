05e53a15365e3b62e6832723e23687c1a14833db
sahil-1012 <sahil10.jai@gmail.com>
2023-10-31 23:16:13 +0530

    Final

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import React, { useMemo } from 'react'
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from 'scenes/homePage/HomePage';
 import LoginPage from 'scenes/loginPage/Login';
 import ProfilePage from 'scenes/profilePage/ProfilePage';
+import { themeSettings } from 'theme';
 
 const App = () => {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
   return (
     <div className='app'>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/profile/:userId' element={<ProfilePage />} />
+        <ThemeProvider theme={theme} >
+          <CssBaseline>
 
-        </Routes>
+            <Routes>
+              <Route path='/' element={<LoginPage />} />
+              <Route path='/home' element={<HomePage />} />
+              <Route path='/profile/:userId' element={<ProfilePage />} />
+            </Routes>
+
+          </CssBaseline>
+        </ThemeProvider>
       </BrowserRouter>
 
 
-    </div>
+    </div >
   )
 };
 
--- client/src/scenes/navbar/Navbar.jsx
+++ client/src/scenes/navbar/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from '@mui/icons-material'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { setMode, setLogout } from "state"
+import { useNavigate } from 'react-router-dom'
+import { Typography, useMediaQuery, useTheme } from '@mui/material'
+import FlexBetween from 'components/FlexBetween'
 
 const Navbar = () => {
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user)
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark
+
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+
     return (
-        <div>Navbar</div>
+        <FlexBetween padding='1rem 6%' backgroundColor={alt}>
+            <FlexBetween gap={"1.75rem"}>
+                <Typography
+                    fontWeight="bold"
+                    fontSize="clamp(1rem, 2rem, 2.25rem)" >
+                    Sociopedia
+                </Typography>
+            </FlexBetween>
+
+        </FlexBetween>
     )
 }
 
-export default Navbar
+export default Navbar;
