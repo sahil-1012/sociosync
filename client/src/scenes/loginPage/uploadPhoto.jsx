bb4e1bf948fa0c56bfbbcfcaee87fa7c827a9ef2
sahil-1012 <sahil10.jai@gmail.com>
2023-11-10 21:58:30 +0530

    Final

--- client/src/scenes/loginPage/uploadPhoto.jsx
+++ client/src/scenes/loginPage/uploadPhoto.jsx
@@ -4,9 +4,9 @@ import FlexBetween from 'components/FlexBetween';
 import React, { useEffect, useState } from 'react';
 import Dropzone from 'react-dropzone';
 
-const UploadPhoto = ({ handleSkip, uploadURL }) => {
+const UploadPhoto = ({ handleSkip }) => {
 
-    // const uploadURL = 'https://sociopedia.s3.us-east-005.backblazeb2.com/654e15dea2cfb26b86e561a6.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=0052c4efc5df2780000000004%2F20231110%2Fus-east-005%2Fs3%2Faws4_request&X-Amz-Date=20231110T113702Z&X-Amz-Expires=900&X-Amz-Signature=217ee4593bc0076754ea0b5a05f698dbe3365bed03414d48d727d12d22b3be6c&X-Amz-SignedHeaders=host&x-id=PutObject'
+    const uploadURL = 'https://sociopedia.s3.us-east-005.backblazeb2.com/image.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=0052c4efc5df2780000000004%2F20231110%2Fus-east-005%2Fs3%2Faws4_request&X-Amz-Date=20231110T162755Z&X-Amz-Expires=900&X-Amz-Signature=90051ce913a4ad597f6163159e0fbc030322fa9b23989c49ec252200aafbea7e&X-Amz-SignedHeaders=host&x-id=PutObject'
     const { palette } = useTheme();
     const [selectedPhoto, setSelectedPhoto] = useState(null);
     const isNonMobile = useMediaQuery("(min-width:600px)")
