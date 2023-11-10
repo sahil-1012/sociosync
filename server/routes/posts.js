f9a6d70b552d72278bb36a6c182d2d153e4a1f14
sahil-1012 <sahil10.jai@gmail.com>
2023-11-10 23:44:02 +0530

    Final

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { verifyToken } = require("../middleware/auth.js");
-const { getFeedPosts, getUserPosts, likePost } = require("../controller/post.js");
+const { getFeedPosts, getUserPosts, likePost, createPost } = require("../controller/post.js");
 
 const router = express.Router();
 
 router.get('/', verifyToken, getFeedPosts);
-app.post("/addPost", verifyToken, createPost)   
+router.post("/addPost", verifyToken, createPost)   
 router.get('/:userId/posts', verifyToken, getUserPosts);
 router.patch('/:postId/like', verifyToken, likePost);
 
