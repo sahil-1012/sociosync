79162f6b479124ad4b2c016ec77eafcf82e71a22
sahil-1012 <sahil10.jai@gmail.com>
2023-11-23 20:35:11 +0530

    Final

--- server/index.js
+++ server/index.js
@@ -16,13 +16,13 @@ const PORT = process.env.PORT || 4001
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
     next();
 });
-app.use(cors())
 
 
 dbConnection();
